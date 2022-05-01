d40966bb676bfaa2cdbdb53018b326f1eb8e52cc
Naysay3r <ilia.mahnach@gmail.com>
2022-05-01 20:08:13 +0300

    feat: realize extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const createAdditionArr = () => {
+      let arr = [];
+      if (options.additionRepeatTimes === undefined) {
+          arr.push(options.addition === undefined ? '' : String(options.addition));
+      } else {
+          for (let i = 0; i < options.additionRepeatTimes; i++) {
+              arr.push(String(options.addition));
+          }
+      }
+      return arr;
+  }
+  const createStrArr = (str, additionArr) => {
+      let arr = [];
+      if (additionArr === undefined) additionArr = '';
+      if (options.repeatTimes === undefined) {
+          arr.push(String(str) + additionArr);
+      } else {
+          for (let i = 0; i < options.repeatTimes; i++) {
+              arr.push(String(str) + additionArr);
+          }
+      }
+      return arr;
+  }
+
+  if (options.separator === undefined) options.separator = '+';
+  if (options.additionSeparator === undefined) options.additionSeparator = '|';
+
+  let additionString = createAdditionArr().join(options.additionSeparator);
+  let strString = createStrArr(str, additionString).join(options.separator);
+  return strString;
 }
 
 module.exports = {
